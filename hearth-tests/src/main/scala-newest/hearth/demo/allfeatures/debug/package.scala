86aa87518cf924132071a3196ad57be2719cf61d
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2026-02-11 19:24:28 +0100

    Minor fixes in comments

--- hearth-tests/src/main/scala-newest/hearth/demo/allfeatures/debug/package.scala
+++ hearth-tests/src/main/scala-newest/hearth/demo/allfeatures/debug/package.scala
@@ -1,6 +1,12 @@
 package hearth.demo.allfeatures
 
+// $COVERAGE-OFF$
 package object debug {
 
+  /** Import [[FastShowPretty.LogDerivation]] in the scope to preview how the derivation is done.
+    *
+    * Put outside of [[FastShowPretty]] companion to prevent the implicit from being summoned automatically!
+    */
   implicit val logDerivationForFastShowPretty: FastShowPretty.LogDerivation = FastShowPretty.LogDerivation
 }
+// $COVERAGE-ON$
--- hearth/src/main/scala-3/hearth/std/extensions/IsValueTypeProviderForOpaque.scala
+++ hearth/src/main/scala-3/hearth/std/extensions/IsValueTypeProviderForOpaque.scala
@@ -24,8 +24,9 @@ final class IsValueTypeProviderForOpaque extends StandardMacroExtension {
         *
         * Strategy:
         *   1. Try `simplified` which reduces TypeRefs to their underlying type when in scope
-        *   2. Try `translucentSuperType` which returns the upper bound of an opaque type
-        *   3. Fall back to inferring from PlainValue ctor (the one that returns A directly, not Either[?, A])
+        *   2. Outside the opaque type's scope
+        *    - try to infer from PlainValue ctor (the one that returns A directly, not Either[?, A])
+        *    - fall back to the underlying type of the opaque type
         *
         * Note: We cannot use `sym.tree` from outside the opaque type's scope because it only shows TypeBounds, not the
         * actual underlying type.
